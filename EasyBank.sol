0a0296075cd63e05e76b7c9d77debcdf1b0b7528
sekkarin <sekkri1234@gmail.com>
2023-09-12 12:55:24 +0700

    fix deposit

--- EasyBank.sol
+++ EasyBank.sol
@@ -3,21 +3,20 @@ pragma solidity ^0.8.9;
 
 contract EasyBank {
     mapping(address => uint256) public balances;
-    address public owner;
+    // address public owner;
     // log
     event LogDepositMade(address indexed accountAddress, uint256 amount);
 
-    constructor() payable {
-        // require(msg.value == 30 ether, "30 ether initial funding required");
-        owner = msg.sender;
-    }
+    // constructor() payable {
+    //     owner = msg.sender;
+    // }
 
     function getBalance() public view returns (uint256) {
         return balances[msg.sender];
     }
 
     function deposit(uint256 money) public payable returns (uint256) {
-        balances[msg.sender] = money;
+        balances[msg.sender] += money;
         emit LogDepositMade(msg.sender, money);
         return balances[msg.sender];
     }
